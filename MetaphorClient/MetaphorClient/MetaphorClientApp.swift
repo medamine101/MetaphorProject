16d37730de44f2e552e92d96b08483acaa29d84b
Mohamed Amine Smires <mohamedamines@outlook.com>
2023-10-01 11:26:41 -0400

    Initial Commit
    Adding all files to upload to GitHub

---
+++ MetaphorClient/MetaphorClient/MetaphorClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaphorClientApp.swift
+//  MetaphorClient
+//
+//  Created by Mohamed Smires on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaphorClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
